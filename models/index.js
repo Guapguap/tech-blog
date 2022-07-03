7a667ac1f931f5d93fdc9ef2efcd936da77da3ba
alberto londono <albertolondono.londono5@gmail.com>
2022-07-03 12:23:49 -0500

    Add index.js with imported models and their associations

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const User = require("./User");
+const Posts = require("./Posts");
+const Comments = require("./Comments");
+
+// Foreign keys set and onDelete: "CASCADE" to remove any child data related to the parent 
+User.hasMany(Posts, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+})
+
+Posts.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+User.hasMany(Comments, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+})
+
+Comments.belongsTo(User, {
+    foreignKey: "user_id"
+})
+
+Posts.hasMany(Comments, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
+})
+
+Comments.belongsTo(Posts, {
+    foreignKey: "post_id"
+})
+
+module.exports = {User, Posts, Comments}
